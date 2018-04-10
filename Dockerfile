f2b5d1c6699313181570f3aa59d55035819fef46
p0lly1 <38227459+p0lly1@users.noreply.github.com>
2018-04-10 14:39:32 -0300

    commit aula 2
    persistencia volumes

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ ENV FLASK_APP=app.py
 WORKDIR /usr/src
 COPY . /usr/src
 RUN pip install -r requirements.txt
-CMD ["flask","run"] 
+CMD ["flask","run", "-h","0.0.0.0"] 
--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+#from flask import Flask
+#app = Flask(__name__)
+
+#@app.route("/")
+#def hello():
+#    return "Hello World!"
+
+
+import logging
+from flask import Flask, request
+from logging.handlers import RotatingFileHandler
+
 app = Flask(__name__)
+
+handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
 
 @app.route("/")
 def hello():
+    app.logger.error(('The referrer was {}'.format(request.referrer)))
     return "Hello World!"
 
