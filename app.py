aab2624f83d1fe0c31c061e57bbf2c11beba7a48
Pollyana <pollyana.souza@serpro.gov.br>
2018-04-11 16:48:23 -0300

    Aula 3 - Exercicio 3

--- app.py
+++ app.py
@@ -6,13 +6,31 @@
 #    return "Hello World!"
 
 
+#import logging
+#from flask import Flask, request
+#from logging.handlers import RotatingFileHandler
+
+#app = Flask(__name__)
+
+#handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+#handler.setLevel(logging.INFO)
+#app.logger.addHandler(handler)
+
+#@app.route("/")
+#def hello():
+#    app.logger.error(('The referrer was {}'.format(request.referrer)))
+#    return "Hello World!"
+
 import logging
 from flask import Flask, request
 from logging.handlers import RotatingFileHandler
 
-app = Flask(__name__)
+from flask.ext.redis import FlaskRedis
 
-handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+app = Flask(__name__)
+redis_store = FlaskRedis(app)
+
+handler = RotatingFileHandler('/tmp/foo.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
